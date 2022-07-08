d6eaa1b1032ea136579253ab8f6dd39443cf2992
usmonovdev <usmonovazizbek15@gmail.com>
2022-07-08 20:15:30 +0500

    img upload added

--- app.js
+++ app.js
@@ -18,3 +18,49 @@ function shareR() {
 
     document.getElementById("form").reset()
 }
+
+function shareL() {
+    let input = document.getElementById("input").value
+    let chatMessage = document.querySelector(".chatBox")
+    let message = document.createElement("span")
+    let div = document.createElement("div")
+    div.setAttribute("class", "spanBoxL")
+    div.appendChild(message)
+    chatMessage.appendChild(div)
+    message.appendChild(document.createTextNode(input))
+
+    if (input.length < 1) {
+        message.style.display = "none"
+        console.log("if")
+    } else {
+        message.style.display = "block"
+        console.log("else")
+    }
+
+    document.getElementById("form").reset()
+}
+
+function sendFile() {
+    let chatMessage = document.querySelector(".chatBox")
+    let img = document.createElement("img")
+    let div = document.createElement("div")
+    div.setAttribute("class", "spanBoxR")
+    div.appendChild(img)
+    chatMessage.appendChild(div)
+
+    let sendFInp = document.getElementById("sendFile")
+
+    sendFInp.addEventListener("change", function() {
+        const choosFile = this.files[0]
+
+        if (choosFile) {
+            const reader = new FileReader()
+
+            reader.addEventListener("load", function() {
+                img.setAttribute("src", reader.result);
+            });
+
+            reader.readAsDataURL(choosFile)
+        }
+    })
+}
